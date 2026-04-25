5b3c766fff7d381b6b1537b6bbecf76b25624f9d
wchengyen <wchengyen@gmail.com>
2026-04-25 12:13:12 +0000

    fix(adapters): defer FeishuAdapter/WeixinAdapter imports until modules exist

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,5 +1,6 @@
 from .base import PlatformAdapter, IncomingMessage, OutgoingPayload
-from .feishu import FeishuAdapter
-from .weixin import WeixinAdapter
+# 后续任务完成后启用:
+# from .feishu import FeishuAdapter
+# from .weixin import WeixinAdapter
 
-__all__ = ["PlatformAdapter", "IncomingMessage", "OutgoingPayload", "FeishuAdapter", "WeixinAdapter"]
+__all__ = ["PlatformAdapter", "IncomingMessage", "OutgoingPayload"]  # , "FeishuAdapter", "WeixinAdapter"
